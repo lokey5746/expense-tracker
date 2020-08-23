d7f8ecaa28e506db1f4b3463bde9edd3162efa48
Lokendra Dangi <pachar.com@gmail.com>
2020-08-24 03:41:10 +0545

    fixed border color

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,7 +4,7 @@ const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <div>
-      <li className="minus">
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
         {transaction.text}{" "}
         <span>
           {sign}${Math.abs(transaction.amount)}
